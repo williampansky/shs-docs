015ef14743de484dd04fc62af664a32d42350d9b
williampansky <williampansky@gmail.com>
2018-06-07 15:01:17 -0500

    - BrowserSync up and running - Starting dev on Sass

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import _ from 'lodash';
-import uikit from 'uikit';
 import prism from './src/js/prism.custom.js';
 import treeview from './src/js/prism-treeview.js';
 import sidebarPosition from './src/js/sidebarposition.js';
